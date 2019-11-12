e7d10f0f4e56b6c44f9510fb159d5fb63cd0f4e8
Reza Teshnizi <rht1369@yahoo.com>
2019-11-12 17:32:26 -0600

    feat(funnel): saving work

--- algorithm/funnel.py
+++ algorithm/funnel.py
@@ -1,30 +1,40 @@
-from triangulation import Triangulation
-from utils.cgal.types import Point
+from algorithm.triangulation import Triangulation
+from utils.cgal.types import Point, convertToPoint
 import utils.cgal.geometry as Geom
 from collections import deque
 
 class Funnel:
-	def __init__(self, apex: Point, dest: Point, triangulation: Triangulation, sleeve: list):
-		self.apex = apex
+	def __init__(self, src: Point, dest: Point, triangulation: Triangulation, sleeve: list):
+		self.src = src
 		self.dest = dest
 		self.tri = triangulation
 		self.sleeve = sleeve
+		self.apexInd = 1
 		self._funnel = deque()
 		self._addFirstTriangleToFunnel()
+		if len(sleeve) == 1: return
 		self._build()
 
 	def _addFirstTriangleToFunnel(self):
-		e = self.tri.getCommonEdge(self.sleeve[i], self.sleeve[i + 1])
+		e = self.tri.getCommonEdge(self.sleeve[0], self.sleeve[1])
 		if not e:
 			raise RuntimeError("Sleeve must have neighboring triangles.")
 		e = list(e)
 		mid = Geom.midpoint(e[0], e[1])
-		if Geom.isToTheRight(self.apex, mid, e[0]):
-			self._funnel.append(e[0])
-			self._funnel.appendleft(e[1])
+		if Geom.isToTheRight(self.src, mid, e[0]):
+			self._funnel.append(convertToPoint(e[0]))
+			self._funnel.appendleft(convertToPoint([1]))
 		else:
-			self._funnel.append(e[1])
-			self._funnel.appendleft(e[0])
+			self._funnel.append(convertToPoint([1]))
+			self._funnel.appendleft(convertToPoint([0]))
 
 	def _build(self):
-		pass
+		for i in range(1, len(self.sleeve) - 1):
+			e = self.tri.getCommonEdge(self.sleeve[i], self.sleeve[i + 1])
+
+	def _updateFunnel(self, candidate):
+		pt = convertToPoint(candidate)
+		if self._funnel[0] == pt or self._funnel[-1] == pt: return
+		for i in range(len(self._funnel)):
+			pass
+
