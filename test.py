a15303d4e5f66dd8831ec15d5ae72b552085d6fe
Reza Teshnizi <rht1369@yahoo.com>
2019-12-26 13:21:53 -0600

    feat(tests): add cli arg for verbosity

--- test.py
+++ test.py
@@ -1,13 +1,22 @@
+import sys
+
 from tests.visibility import TestVisibility
 from tests.tighten import TestTighten
+from tests.unitTest import Verbosity
 
-def main():
+def main(verbosity=Verbosity.NONE):
 	unitTests = [TestVisibility(), TestTighten()]
 	for test in unitTests:
 		print("Running %s: %d test cases" % (test.name, test.numTests))
-		result = test.run()
+		result = test.run(verbosity)
 		print(result)
 		print()
 
 if __name__ == '__main__':
-	main()
+	verbosity = Verbosity.NONE
+	if "-v1" in sys.argv: verbosity = Verbosity.LEAST
+	elif "-v2" in sys.argv: verbosity = Verbosity.MEDIUM
+	elif "-v3" in sys.argv: verbosity = Verbosity.MOST
+	print("Verbosity = %s" % str(verbosity)[10:])
+	print()
+	main(verbosity)
