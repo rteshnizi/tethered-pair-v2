b6ac4dd7cbe014a6205bee34f7ce9a1e28a0cf3d
Reza Teshnizi <rht1369@yahoo.com>
2019-12-26 20:38:51 -0600

    fix(aStar): Fix destination detection

--- algorithm/aStar.py
+++ algorithm/aStar.py
@@ -1,8 +1,7 @@
 import utils.cgal.geometry as Geom
 from utils.vertexUtils import convertToPoint, getClosestVertex, almostEqual, removeRepeatedVertsOrdered
-from algorithm.visibility import findGaps
 from algorithm.node import Node
-from algorithm.cableAlgorithms import tightenCable
+from algorithm.cable import tightenCable, getLongCable
 from model.modelService import Model
 from utils.priorityQ import PriorityQ
 from utils.cgal.types import Polygon
@@ -18,8 +17,6 @@ def aStar() -> Node:
 		if isAtDestination(n):
 			print("At Destination")
 			return n # For now terminate at first solution
-		# VA = findGaps(n.cable[0], model.robots[0])
-		# VB = findGaps(n.cable[-1], model.robots[-1])
 		for va in n.cable[0].gaps:
 			for vb in n.cable[-1].gaps:
 				# For now I deliberately avoid cross movement because it crashes the triangulation
@@ -37,11 +34,9 @@ def aStar() -> Node:
 def isThereCrossMovement(cable, dest1, dest2):
 	# I've also included the case where the polygon is not simple
 	p = Polygon()
-	p.push_back(convertToPoint(dest1))
-	for v in cable:
+	for v in getLongCable(cable, dest1, dest2):
 		p.push_back(convertToPoint(v))
-	p.push_back(convertToPoint(dest2))
 	return not p.is_simple()
 
 def isAtDestination(n) -> bool:
-	return n.cable[0].name == model.robots[0].destination.name and n.cable[-1].name == model.robots[-1].destination.name
+	return convertToPoint(n.cable[0]) == convertToPoint(model.robots[0].destination) and convertToPoint(n.cable[-1]) == convertToPoint(model.robots[-1].destination)
